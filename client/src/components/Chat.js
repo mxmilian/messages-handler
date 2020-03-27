76863b01fa30859c40fe1608d2ae3aaf1858e855
s450026 <maksymilian.kierski@gmail.com>
2020-03-27 17:00:57 +0100

    Add client-io and handle route in chat view

--- client/src/components/Chat.js
+++ client/src/components/Chat.js
@@ -1,7 +1,14 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import querystring from 'query-string';
+import socket from 'socket.io-client';
+const Chat = ({ location }) => {
+  useEffect(() => {
+    const data = querystring.parse(location.search);
+    console.log(location.search);
+    console.log(data);
+  });
 
-const Chat = () => (
-  <h1>Hello is chat here!</h1>
-);
+  return <h1>Chat</h1>;
+};
 
 export default Chat;
