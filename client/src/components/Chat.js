50da2b1085c38a45debb8b0d7b8892df9227d762
s450026 <maksymilian.kierski@gmail.com>
2020-03-27 16:12:18 +0100

    Initliaze a client

---
+++ client/src/components/Chat.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Chat = () => (
+  <h1>Hello is chat here!</h1>
+);
+
+export default Chat;
---
+++ client/src/components/Join.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Join = () => (
+  <h1>Hello is join here!</h1>
+);
+
+export default Join;
