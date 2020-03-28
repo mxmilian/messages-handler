df40a683e3852f347a60de5cb090c51376f2ed4d
s450026 <maksymilian.kierski@gmail.com>
2020-03-28 22:40:26 +0100

    Initialize arduino handler server

--- client/src/components/Join.js
+++ client/src/components/Join.js
@@ -7,15 +7,17 @@ const Join = () => {
     <div>
       <h1>Do something with arduino!</h1>
       <input placeholder="Name" type="text" onChange={e => setName(e.target.value)} />
-      <select onChange={e => setRoom(e.target.value)}>
-        <option value="arduino">Arduino</option>
-        <option value="talk">Just talk</option>
-      </select>
+      <input placeholder="Room" type="text" onChange={e => setRoom(e.target.value)} />
       <Link onClick={e => (!name || !room) ? e.preventDefault() : null} to={`/chat?name=${name}&room=${room}`}>
         <button type="submit">Join to arduino room</button>
       </Link>
     </div>
   )
+
+    /*<select onChange={e => setRoom(e.target.value)}>
+        <option value="arduino">Arduino</option>
+    </select>
+     */
 };
 
 export default Join;
