7448fa14d5adc8dd43003eb033748dee8c2c5381
s450026 <maksymilian.kierski@gmail.com>
2020-03-28 16:16:27 +0100

    Specify rooms

--- client/src/components/Join.js
+++ client/src/components/Join.js
@@ -2,12 +2,15 @@ import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 const Join = () => {
   const [name, setName] = useState('');
-  const [room, setRoom] = useState('');
+  const [room, setRoom] = useState('arduino');
   return(
     <div>
       <h1>Do something with arduino!</h1>
       <input placeholder="Name" type="text" onChange={e => setName(e.target.value)} />
-      <input placeholder="Room" type="text" onChange={e => setRoom(e.target.value)} />
+      <select onChange={e => setRoom(e.target.value)}>
+        <option value="arduino">Arduino</option>
+        <option value="talk">Just talk</option>
+      </select>
       <Link onClick={e => (!name || !room) ? e.preventDefault() : null} to={`/chat?name=${name}&room=${room}`}>
         <button type="submit">Join to arduino room</button>
       </Link>
