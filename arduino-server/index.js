9d98c5beb42fdadddeb339bd025e2de356a435a5
s450026 <maksymilian.kierski@gmail.com>
2020-04-01 13:53:14 +0200

    Add arduino funcionality to arduino server

--- arduino-server/index.js
+++ arduino-server/index.js
@@ -1,17 +1,28 @@
 const io = require('socket.io-client');
+const five = require('johnny-five');
 
 const ENDPOINT = 'https://messages-handler.herokuapp.com/';
 const socket = io(ENDPOINT);
+const board = new five.Board();
 
 socket.on('connect', () => {
   console.log(`Connected with ${ENDPOINT}`);
 });
 
 socket.emit('join', {
-  name: 'xD',
+  name: 'ardunio-bot',
   room: 'room'
 }, () => {});
 
-socket.on('message',  message => {
-  console.log(message.text);
+board.on('ready', () => {
+  const led = new five.Led(12);
+  socket.on('message',  message => {
+    const mess = message.text.toLowerCase();
+    if(mess === '!ledon'){
+      led.on();
+    }
+    if(mess === '!ledoff') {
+      led.off();
+    }
+  });
 });
